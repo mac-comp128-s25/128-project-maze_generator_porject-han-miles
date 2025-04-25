1b5a7d7245f162f27c1800399c3dc49ef0bcaab2
shufubianyibo <hsun1@macalester.edu>
2025-04-25 09:49:51 -0500

    use sets&complete vis

--- src/Maze.java
+++ src/Maze.java
@@ -4,16 +4,15 @@ public class Maze {
     private final CanvasWindow canvas;
     private static final int CANVAS_WIDTH = 800;
     private static final int CANVAS_HEIGHT = 600;
-    private static final int NODE_RADIUS = 10;
+    private static final int NODE_RADIUS = 30;
     public Maze(){
         canvas = new CanvasWindow("Maze", 800, 600);
         /* generate a maze using Prim's algorithm */
         int gridSize = 10;
         PrimsAlgorithmGenerator generator = new PrimsAlgorithmGenerator(gridSize); 
         generator.generateMaze();
-        generator.addRandomEdges(0.15);
-        var nodePositions = generator.generateNodePositions(CANVAS_WIDTH, CANVAS_HEIGHT);
-        generator.drawMaze(canvas, nodePositions, NODE_RADIUS);
+        generator.addRandomEdges(0.05);
+        generator.drawMaze(canvas, CANVAS_WIDTH, CANVAS_HEIGHT, NODE_RADIUS);
     }
 
     
